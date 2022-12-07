441596c798e593edd1f6ab40845ffb6907c2daec
hzhou74 <h9zhou.ucsd@gmail.com>
2022-12-06 17:58:35 -0800

    Update the port for heroku - try a different port number

--- server.ts
+++ server.ts
@@ -2,14 +2,14 @@
  * @file Implements an Express Node HTTP server.
  */
 // import UserDao from "./daos/UserDao";
-import UserController from "./controllers/UserController";
+// import UserController from "./controllers/UserController";
 import express, {Request, Response} from 'express';
-import mongoose from "mongoose";
-import TuitController from "./controllers/TuitController";
+// import mongoose from "mongoose";
+// import TuitController from "./controllers/TuitController";
 // import TuitDao from "./daos/TuitDao";
-import FollowController from "./controllers/FollowController";
-import BookmarkController from "./controllers/BookmarkController";
-import MessageController from "./controllers/MessageController";
+// import FollowController from "./controllers/FollowController";
+// import BookmarkController from "./controllers/BookmarkController";
+// import MessageController from "./controllers/MessageController";
 
 
 
@@ -20,16 +20,16 @@ const cors = require('cors')
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    autoIndex: false,
-    maxPoolSize: 10,
-    serverSelectionTimeoutMS: 5000,
-    socketTimeoutMS: 45000,
-    family: 4
-}
+//
+// const options = {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     autoIndex: false,
+//     maxPoolSize: 10,
+//     serverSelectionTimeoutMS: 5000,
+//     socketTimeoutMS: 45000,
+//     family: 4
+// }
 
 
 // let sess = {
@@ -49,11 +49,11 @@ const options = {
 //mongodb+srv://cs5500fa22:HqisGljV7ra4jAy2@cluster0.bkwci2f.mongodb.net/CS5520Fa22_Heye
 //mongodb+srv://cs5500fa22:HqisGljV7ra4jAy2@cluster0.bkwci2f.mongodb.net/?retryWrites=true&w=majority
 
-// // const MONGODB_URI = "mongodb+srv://cs5500fa22:HqisGljV7ra4jAy2@cluster0.bkwci2f.mongodb.net/CS5520Fa22_Heye?retryWrites=true&w=majority"
+// const MONGODB_URI = "mongodb+srv://cs5500fa22:HqisGljV7ra4jAy2@cluster0.bkwci2f.mongodb.net/CS5520Fa22_Heye?retryWrites=true&w=majority"
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://cs5500fa22:HqisGljV7ra4jAy2@cluster0.bkwci2f.mongodb.net/CS5520Fa22_Heye', options);
-// //
-// // // const db = conn.db('CS5520Fa22_Heye')
-// //
+//
+// // const db = conn.db('CS5520Fa22_Heye')
+//
 // const userController = UserController.getInstance(app);
 // const tuitController = TuitController.getInstance(app);
 // const followController = FollowController.getInstance(app);
@@ -71,10 +71,9 @@ app.get('/hello', (req: Request, res: Response) =>
  * but use environment variable PORT on Heroku if available.
  */
 const PORT = 4000;
-const port = process.env.PORT || PORT
-app.listen(port)
+// const port = process.env.PORT || PORT
+app.listen(process.env.PORT || PORT);
 
-console.log('sever running on port',port);
 
 // app.listen(port, err, () => {
 //     if (err) return console.log(err);
