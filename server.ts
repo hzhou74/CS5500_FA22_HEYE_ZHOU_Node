c988aa2365f9e6313b2dccce572cbf8666860572
hzhou74 <h9zhou.ucsd@gmail.com>
2022-12-10 09:18:10 -0800

    minor bug fix for A4

--- server.ts
+++ server.ts
@@ -4,11 +4,8 @@
 
 import express, {Request, Response} from 'express';
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
-// import UserDao from "./daos/UserDao";
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
-// import TuitDao from "./daos/TuitDao";
 import FollowController from "./controllers/FollowController";
 import BookmarkController from "./controllers/BookmarkController";
 import MessageController from "./controllers/MessageController";
@@ -48,23 +45,25 @@ app.use(session(sess));
 app.use(express.json());
 
 
-//
-// const options = {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     autoIndex: false,
-//     maxPoolSize: 10,
-//     serverSelectionTimeoutMS: 5000,
-//     socketTimeoutMS: 45000,
-//     family: 4
-// }
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex: false,
+    maxPoolSize: 10,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+    family: 4
+}
 
 // 'mongodb+srv://cs5500fa22:HqisGljV7ra4jAy2@cluster0.bkwci2f.mongodb.net/test'
-// mongodb://localhost:27017/tuiter'
 //mongodb+srv://cs5500fa22:HqisGljV7ra4jAy2@cluster0.bkwci2f.mongodb.net/CS5520Fa22_Heye
 //mongodb+srv://cs5500fa22:HqisGljV7ra4jAy2@cluster0.bkwci2f.mongodb.net/?retryWrites=true&w=majority
+// mongoose.connect('mongodb+srv://ujjval:ujjval@cluster0.zwdxz.mongodb.net/whiteboard?retryWrites=true&w=majority&ssl=true');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://cs5500fa22:HqisGljV7ra4jAy2@cluster0.bkwci2f.mongodb.net/CS5520Fa22_Heye');
+// mongoose.connect('mongodb+srv://cs5500fa22:HqisGljV7ra4jAy2@cluster0.bkwci2f.mongodb.net/CS5520Fa22_Heye?retryWrites=true&w=majority&ssl=true');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb+srv://cs5500fa22:HqisGljV7ra4jAy2@whiteboard.4xo1zxp.mongodb.net/whiteboard?retryWrites=true&w=majority',options);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://cs5500fa22:HqisGljV7ra4jAy2@cluster0.bkwci2f.mongodb.net/CS5520Fa22_Heye');
 
 const userController = UserController.getInstance(app);
 const tuitController = TuitController.getInstance(app);
